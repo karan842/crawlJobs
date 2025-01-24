157ab2c0e7cde2b2ec8b4b1376d90c4fd0e307cc
Karan Shingde <karanshingde@gmail.com>
2025-01-24 20:33:03 +0530

    logging added

---
+++ src/utils/logging.py
@@ -0,0 +1,10 @@
+import logging
+import os
+
+# Set up basic configuration for logging
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+
+# Now you can import 'logger' in other modules
+logger = logging.getLogger(__name__)
